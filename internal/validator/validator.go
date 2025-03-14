6ffdc3da8b8fc3cf83f37941fc18ba71dfb62a26
daulet-omarov <dauka.omar04@gmail.com>
2025-03-14 16:29:40 +0500

    switch PemitedInt -> PermitedValue

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -49,7 +49,7 @@ func MinChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) >= n
 }
 
-func PermittedInt(value int, permittedValues ...int) bool {
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
 	for _, permittedValue := range permittedValues {
 		if value == permittedValue {
 			return true
